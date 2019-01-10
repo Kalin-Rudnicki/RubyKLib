c3f5ab38ac1a9e79bd2cb265ebab4ad9cd5af8b9
Kalin-Rudnicki <deaddorks@gmail.com>
2019-01-10 04:27:35 -0600

    added 'map_to_h'

--- src/utils/general/ArrayUtils.rb
+++ src/utils/general/ArrayUtils.rb
@@ -1,5 +1,8 @@
 
 require 'set'
+Dir.chdir(File.dirname(__FILE__)) do
+	require './../validation/HashNormalizer'
+end
 
 class Array
 
@@ -12,6 +15,23 @@ class Array
 			seen << element
 		end
 		dups.to_a
+	end
+	
+	def map_to_h(hash_args = {}, &block)
+		raise ArgumentError.new("You must supply a block to this method") unless block_given?
+		hash_args = KLib::HashNormalizer.normalize(hash_args) do |norm|
+			norm.mode.default_value(:value).enum_check(:key, :value, :array)
+		end
+		case hash_args[:mode]
+			when :key
+				self.map { |e| [e, block.call(e)] }.to_h
+			when :value
+				self.map { |e| [block.call(e), e] }.to_h
+			when :array
+				self.map { |e| block.call(e) }.to_h
+			else
+				raise 'what is happening...'
+		end
 	end
 
 end
