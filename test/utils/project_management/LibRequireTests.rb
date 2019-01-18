d11139cde2eb3ae2079c94e9b831a434281f66f5
Kalin-Rudnicki <deaddorks@gmail.com>
2019-01-17 23:00:28 -0600

    removed KLib require-esq stuff, moving towards making a gem

--- test/utils/project_management/LibRequireTests.rb
+++
@@ -1,8 +0,0 @@
-
-Dir.chdir(File.dirname(__FILE__)) do
-	require './../../../src/utils/project_management/LibRequire'
-end
-
-# lib_require('utils/version_compat/BasicObject')
-
-require('version_compat/BasicObject')
