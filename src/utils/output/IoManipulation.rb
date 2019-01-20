4d00d2a5cad94b804cf47b41f41fc3ce83a76c7f
Kalin-Rudnicki <deaddorks@gmail.com>
2019-01-19 20:36:31 -0600

    grab STDOUT and STDERR as well

--- src/utils/output/IoManipulation.rb
+++ src/utils/output/IoManipulation.rb
@@ -1,12 +1,17 @@
 
+Dir.chdir(File.dirname(__FILE__)) do
+	require './../validation/ArgumentChecking'
+end
+
 module KLib
 
 	module IoManipulation
 		
 		class << self
 		
-			def snatch_io(&block)
+			def snatch_io(grab_constants = true, &block)
 				raise ArgumentError.new('You must supply a block to this method') unless block_given?
+				ArgumentChecking.boolean_check(grab_constants, 'grab_constants')
 				
 				stdout = $stdout
 				stderr = $stderr
@@ -16,6 +21,13 @@ module KLib
 				
 				$stdout = out_write
 				$stderr = err_write
+				
+				if grab_constants
+					Object.send(:remove_const, :STDOUT)
+					Object.const_set(:STDOUT, out_write)
+					Object.send(:remove_const, :STDERR)
+					Object.const_set(:STDERR, err_write)
+				end
 				
 				begin
 					block.call
@@ -29,6 +41,13 @@ module KLib
 					
 					$stdout = stdout
 					$stderr = stderr
+					
+					if grab_constants
+						Object.send(:remove_const, :STDOUT)
+						Object.const_set(:STDOUT, stdout)
+						Object.send(:remove_const, :STDERR)
+						Object.const_set(:STDERR, stderr)
+					end
 				end
 				
 				raise error unless error.nil?
