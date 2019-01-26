6e85ce13e7a6145979890257db032ba09bb84ba0
Kalin-Rudnicki <deaddorks@gmail.com>
2019-01-26 10:28:20 -0600

    removed 'Logging' middle module improved aliasing and coloring method for log-levels added method missing to call log as log level

--- test/utils/output/LoggerTests.rb
+++ test/utils/output/LoggerTests.rb
@@ -3,7 +3,5 @@ Dir.chdir(File.dirname(__FILE__)) do
 	require './../../../src/utils/output/Logger'
 end
 
-logger = KLib::Logging::Logger.new(:log_tolerance => :never, :display_time => false)
+logger = KLib::Logger.new(:log_tolerance => :never, :display_time => false)
 logger.set_log_tolerance(:print, :rule => :cuz)
-
-logger.valid_levels.each { |level| logger.log(level, "Logging at: '#{level}'", :rule => :cuz) }
