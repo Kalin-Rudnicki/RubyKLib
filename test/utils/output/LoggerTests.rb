e2de94d4f71f9d4bc595ff696a3a9819db7c9ba7
Kalin-Rudnicki <deaddorks@gmail.com>
2019-11-05 13:26:57 -0600

    Added very basic logger test

--- test/utils/output/LoggerTests.rb
+++ test/utils/output/LoggerTests.rb
@@ -3,5 +3,9 @@ Dir.chdir(File.dirname(__FILE__)) do
 	require './../../../src/utils/output/Logger'
 end
 
-logger = KLib::Logger.new(:log_tolerance => :never, :display_time => false)
+logger = KLib::Logger.new(:log_tolerance => :always, :display_time => false)
 logger.set_log_tolerance(:print, :rule => :cuz)
+
+logger.print("Hello")
+logger.debug("Hello 2.0", rule: :cuz)
+logger.print("Hello 2.1", rule: :cuz)
