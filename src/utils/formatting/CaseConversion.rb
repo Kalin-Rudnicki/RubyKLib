0174e95d26836caeb0ec0fcafc7ecf4f9e09c7b4
Kalin-Rudnicki <deaddorks@gmail.com>
2019-11-06 10:53:46 -0600

    Improved CaseConversion

--- src/utils/formatting/CaseConversion.rb
+++ src/utils/formatting/CaseConversion.rb
@@ -1,16 +1,19 @@
 
+require_relative '../validation/ArgumentChecking'
+
 class String
 	
 	def to_snake
 		self.gsub(/::/, '/').
 			gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+			gsub(/([a-z])(\d)/,'\1_\2').
 			gsub(/([a-z\d])([A-Z])/,'\1_\2').
 			tr("-", "_").
 			downcase
 	end
 	
 	def to_camel(downcase_first_char = true)
-		raise ArgumentError.new("Parameter 'downcase_first_char' must be one of [true, false].") unless downcase_first_char == true || downcase_first_char == false
+		KLib::ArgumentChecking.type_check(downcase_first_char, :downcase_first_char, Boolean)
 		split = self.split('_').each { |str| str[0] = str[0].upcase if str.length > 0 }
 		split[0][0] = split[0][0].downcase if downcase_first_char && split.length > 0 && split[0].length > 0
 		split.join('')
