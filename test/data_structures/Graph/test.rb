57b01c40e03c2a653006f5aa631e9e05735ba1be
Kalin-Rudnicki <deaddorks@gmail.com>
2019-12-03 13:46:52 -0600

    newest version of gem, with finished Graph

--- test/data_structures/Graph/test.rb
+++ test/data_structures/Graph/test.rb
@@ -4,5 +4,5 @@ require_relative '../../../src/data_structures/Graph'
 Dir.chdir(File.dirname(__FILE__)) do
 	graph = KLib::Graph.read('test.graph')
 	puts(graph)
-	graph.write('test_ok.graph/ok')
+	graph.write('test_ok.graph')
 end
